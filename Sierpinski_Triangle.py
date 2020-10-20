695d41865b32f1c497fb7ed020edd2b9f5ac820f
@NekkittAY <37750010+NekkittAY@users.noreply.github.com>
2020-10-20 21:19:16 +0200

    Create Sierpinski_Triangle.py

---
+++ Sierpinski_Triangle.py
@@ -0,0 +1,16 @@
+axiom = input()
+itr = int(input())
+tempStr = ""
+for i in range(1, itr+1):
+    ln = len(axiom)
+    for j in range(0, ln):
+        if axiom[j] == "A":
+            tempStr += "B-A-B"
+        elif axiom[j] == "B":
+            tempStr += "A+B+A"
+        elif axiom[j] == "+":
+            tempStr += "+"
+        elif axiom[j] =="-":
+            tempStr += "-"
+    axiom, tempStr = tempStr, ""
+print(axiom)
