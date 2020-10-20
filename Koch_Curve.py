807abc3c76eea0f63ce806766586c82e46b654d4
@NekkittAY <37750010+NekkittAY@users.noreply.github.com>
2020-10-20 21:22:24 +0200

    Create Koch_Curve.py

---
+++ Koch_Curve.py
@@ -0,0 +1,14 @@
+axiom = input()
+itr = int(input())
+tempStr = ""
+for i in range(1, itr+1):
+    ln = len(axiom)    
+    for j in range(0, ln):
+        if axiom[j] == "+":
+            tempStr += "+"
+        elif axiom[j] == "-":
+            tempStr += "-"
+        else:
+            tempStr += "F+F-F-F+F"
+    axiom, tempStr = tempStr, ""
+print(axiom)
